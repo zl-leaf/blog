1cee2e1fbdfc59027835a0622acc09906d0b7ac5
叶志良 <leaf@yezhiliangdeMacBook-Air.local>
2018-02-28 18:04:05 +0800

    修改样式，增加搜索组件与导航栏组件的交互

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
-import '../css/style.css'
+import '../css/style.css';
+
+window.onload = function() {
+    document.getElementById('nav-search-btn').onclick = function() {
+      var searchForm = document.getElementById('search-form-wrap');
+      searchForm.style.right='0px';
+      searchForm.style.opacity=1;
+    }
+
+    var navBtn = document.getElementById('main-nav-toggle');
+    navBtn.onclick = function() {
+      var isOpenNav = navBtn.getAttribute('open');
+      navBtn.setAttribute('open', isOpenNav^1);
+      var navLinkList = document.getElementsByClassName('main-nav-link');
+      for (var i = navLinkList.length - 1; i >= 0; i--) {
+        var navLink = navLinkList[i];
+        if (isOpenNav == 1) {
+          navLink.style.display = 'none';
+        } else {
+          navLink.style.display = 'block';
+        }
+      }
+    }
+}
