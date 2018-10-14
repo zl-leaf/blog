ccfa4d56708f040ef634837c5b152e324ca514f9
叶志良 <leaf@yezhiliangdeMacBook-Air.local>
2018-10-14 18:47:45 +0800

    更新article模块接口

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,13 +1,13 @@
 var config = {
     host: "http://www.yipzale.me",
     article: {
-        list_url: "http://api.yipzale.me/api/article",
-        recent_url: "http://api.yipzale.me/api/article?sortby=updated_at&order=desc&limit=10",
-        meta_url: "http://api.yipzale.me/api/article/@{slug}",
-        content_url: "http://api.yipzale.me/api/article/@{slug}/content"
+        list_url: "http://api.yipzale.me/blog/article",
+        recent_url: "http://api.yipzale.me/blog/article?sortby=updated_at&order=desc&limit=10",
+        meta_url: "http://api.yipzale.me/blog/article/@{slug}",
+        content_url: "http://api.yipzale.me/blog/article/@{slug}/content"
     },
     archives: {
-        list_url: "http://api.yipzale.me/api/archive?_open=1&_with=articles"
+        list_url: "http://api.yipzale.me/blog/archive?_open=1&_with=articles"
     },
     oauth: {
         authorizeUrl: "http://security.yipzale.me/oauth/authorize",
